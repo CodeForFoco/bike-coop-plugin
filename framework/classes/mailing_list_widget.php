d3210b5f4232e577687860e49b10a472a09d16db
David Hayes <git@davidbhayes.com>
2016-11-17 17:54:44 -0700

    Creating basic class for Widget--I think it'll match Darlington's autoloader

---
+++ framework/classes/mailing_list_widget.php
@@ -0,0 +1,44 @@
+<?php
+
+class FCBDMailingList extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		$widget_ops = array( 
+			'classname' => 'FCBDMailingList',
+			'description' => 'To load the mailing list',
+		);
+		parent::__construct( 'FCBDMailingList', 'Mailing List Widget', $widget_ops );
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		do_shortcode('fcbc_mailing_list');
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		// outputs the options form on admin
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved
+	}
+}
