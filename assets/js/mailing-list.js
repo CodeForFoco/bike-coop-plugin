2919ad6e0d2cc05f48f88b50e58d9427d60867f6
David Hayes <git@davidbhayes.com>
2016-12-15 18:44:51 -0700

    Making button not submit on mailing form

--- assets/js/mailing-list.js
+++ assets/js/mailing-list.js
@@ -3,15 +3,20 @@
     "use strict";
 
     $(document).ready( function(){
-    	var data = {
-			'action': 'fcbc_mailing_list',
-			'email': $('.mailing-list-email').val();
-		};
+    	
+    	$('.mailing-list-form-button').click(
 
-		// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
-		jQuery.post(ajaxurl, data, function(response) {
-			alert('Got this from the server: ' + response);
-		});
+	    	var data = {
+				'action': 'fcbc_mailing_list',
+				'email': $('.mailing-list-email').val();
+			};
+
+			// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+			jQuery.post(ajaxurl, data, function(response) {
+				alert('Got this from the server: ' + response);
+			});
+
+    	);
     } ); 
  
 })(jQuery);
