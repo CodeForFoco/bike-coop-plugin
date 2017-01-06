5181c60a0ed7fbacd3ed0e2c776dd43100692d56
RADD Creative <radd@raddcreative.com>
2017-01-05 18:09:44 -0700

    working on events

--- framework/modules/events/views/shortcodes/upcoming-events.php
+++ framework/modules/events/views/shortcodes/upcoming-events.php
@@ -26,21 +26,29 @@
         <?php while($events_query->have_posts()): $events_query->the_post(); ?>
         <div class="event-wrapper">
             <div class='event'>
-                <div class="card card-inverse">
+                <?php
+                $url = get_post_meta(get_the_ID(), 'awesome_events_details_url', true);
+
+                if(empty(trim($url)))
+                $url = get_the_permalink();
+                ?>
+                <a class="card card-inverse" href="<?php echo $url; ?>">
                   <?php if( has_post_thumbnail() ): ?>
                   <?php
                     $src = get_the_post_thumbnail_url();
                     $src = aq_resize( $src, 400, 440, true, true, true );
                   ?>
-                  <img class="card-img" src="<?php echo $src; ?>" alt="Upcoming Event">
+
+                      <img class="card-img" src="<?php echo $src; ?>" alt="Upcoming Event">
                  <?php endif; ?>
                   <div class="card-img-overlay">
-                    <h4 class="card-title"><?php //the_title();?></h4>
+                      <div class="inner">
+                        <h4 class="card-title"><?php the_title();?></h4>
+                      </div>
                     <!--<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>-->
                   </div>
-                </div>
-                
+                </a>
             </div>
         </div>
         <?php endwhile; ?>
