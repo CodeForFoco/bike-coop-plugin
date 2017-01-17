49cc07188bb04cc6d3d1aa452fa4b0e2fc23abde
Jason Lampel <travelampel@gmail.com>
2017-01-16 17:49:06 -0700

    Slider: wrap in accidentally-removed <header> tag

--- framework/modules/slider/views/shortcodes/slider.php
+++ framework/modules/slider/views/shortcodes/slider.php
@@ -7,31 +7,33 @@
         'meta_key'      =>  'slider-slide-position'
     );
 ?>
-<div class='fcbc-slider-wrapper'>
-  <div class='fcbc-slider'>
-    <?php
-      $slides = get_posts($args);
-      $shortcodes = BC_Slide_Post_Type::get_slides_for_slider();
-      foreach($slides as $slide): 
-        if ($shortcodes !== '' && strrpos($shortcodes, $slide->post_name) === FALSE){
-          continue;
-        } 
-    ?>        
-    <div class='fcbc-slider-wrapper' style="background-image:url(<?php echo get_the_post_thumbnail_url($slide->ID); ?>)">
-      <div class="tagline">
-        <div class="row">
-          <h1><?php echo $slide->post_title; ?></h1>
-          <h4 class="subheader"><?php echo get_the_excerpt($slide->ID); ?></h4>
-          <a role="button" class="download large button sites-button same-page-link" 
-            href="<?php echo get_post_meta($slide->ID, 'slider-button-url', true); ?>">
-            <?php echo get_post_meta($slide->ID, 'slider-button-text', true); ?>
-          </a>
+<header id="front-hero" role="banner">
+  <div class='fcbc-slider-wrapper'>
+    <div class='fcbc-slider'>
+      <?php
+        $slides = get_posts($args);
+        $shortcodes = BC_Slide_Post_Type::get_slides_for_slider();
+        foreach($slides as $slide): 
+          if ($shortcodes !== '' && strrpos($shortcodes, $slide->post_name) === FALSE){
+            continue;
+          } 
+      ?>        
+      <div class='fcbc-slider-wrapper' style="background-image:url(<?php echo get_the_post_thumbnail_url($slide->ID); ?>)">
+        <div class="tagline">
+          <div class="row">
+            <h1><?php echo $slide->post_title; ?></h1>
+            <h4 class="subheader"><?php echo get_the_excerpt($slide->ID); ?></h4>
+            <a role="button" class="download large button sites-button same-page-link" 
+              href="<?php echo get_post_meta($slide->ID, 'slider-button-url', true); ?>">
+              <?php echo get_post_meta($slide->ID, 'slider-button-text', true); ?>
+            </a>
+          </div>
         </div>
       </div>
+      <?php endforeach; ?>
     </div>
-    <?php endforeach; ?>
   </div>
-</div>
+</header>
 
 <script type="text/javascript">
     jQuery(document).ready(function($){
